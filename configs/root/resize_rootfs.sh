2265c7ee0cb48d31ad0296087ceb556e4de33612
ELAR Systems <sghazagh@yahoo.com>
2015-06-26 15:59:24 +1000

    Update resize_rootfs.sh

--- configs/root/resize_rootfs.sh
+++ configs/root/resize_rootfs.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
+#Read the BOOTSIZE (VFAT Partition Size) from the file. This size set by user inside the build image scripts
+file="/root/vfat_part_size" 
+name=$(cat "$file")        
+
+BOOTSIZE=$(cat "$file")
+EXT4_STAR_SECTOR=($BOOTSIZE+1)*1024*1024)/512
+
 fdisk /dev/mmcblk0 <<EOF
 d
 2
 n
 p
 2
-133120
+$EXT4_START_SECTOR
 
 p
 w
 EOF
+
+#Delete/clean the file that keeps the value of BOOTSIZE (VFAT Partition)
+rm -f  $file
 
 partprobe /dev/mmcblk0
 
